dd1939af216a97d43d619752602f9726ac764998
0xNoo3 <thisisforlinuxonly@gmail.com>
2025-07-23 02:09:53 +0500

    Fixing 3rd Deployment issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,6 @@ const Header = () => {
     <header className={headerClass}>
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
-
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
               {siteMetadata.headerTitle}
