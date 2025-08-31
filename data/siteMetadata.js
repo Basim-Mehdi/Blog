fa352d49933b875c96ce51de0e5055518510a1bc
Basim-Mehdi <232114@students.au.edu.pk>
2025-08-31 23:13:00 +0500

    C00K3D-CTF-Pwn

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/MyLogo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/MyLogo.png`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'basimraza9@gmail.com',
   github: 'https://github.com/Basim-Mehdi/',
